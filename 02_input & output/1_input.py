9a044ad9ca27943255cfc2c0760ed56de0fd889c
arpan <jagtaparpan3@gmail.com>
2026-08-03 16:05:25 +0530

    Some basic input codes

---
+++ 02_input & output/1_input.py
@@ -0,0 +1,4 @@
+Degree = input("Enter your degree name:")
+College_Name = input("Enter your College Name:")
+print("Currently I am pursuing", Degree)
+print("My college name is", College_Name)
---
+++ 02_input & output/input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name ")
+print("hello!", name, "!Welcome")
+roll_no = input("enter your roll number")
+print("my roll number", roll_no, "!")
