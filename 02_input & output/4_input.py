1538fa710dbd1f10d6cb6014480bb9dcd37ad201
arpan <jagtaparpan3@gmail.com>
2026-08-06 01:10:11 +0530

    string input to an integer

---
+++ 02_input & output/4_input.py
@@ -0,0 +1,30 @@
+num = (int(input("Enter your contact number:-")))
+print(num)
+print(type(num))
+
+num = (float(input("Enter weight :-")))
+print(num)
+print(type(num))
+
+s = '50'
+num = int(s)
+print(num)
+
+a = 100
+print(a)
+
+num = int("123")
+print(num)
+
+num1 = float("45.908")
+print(num1)
+
+text = 123
+print(text)
+
+text1 = str(1234)
+print(text1)
+
+num = int(60.70)
+print(num)
+
