ed23eb7a9946754d941a126de5d3e7841b5ee8d8
arpan <jagtaparpan3@gmail.com>
2026-08-06 00:37:38 +0530

    Some basic input codes

---
+++ 02_input & output/3_input.py
@@ -0,0 +1,9 @@
+a = (int(input("What is your Age :-")))
+b = (float(input("What is your Weight :- ")))
+c = "This is my personal information"
+d , f = input("Enter your 2-vehicle number :-").split()
+print("Age in number/integer" , a)
+print("Weight in KG" ,b)
+print(c)
+print("my bike number" ,d , "my car number" , f)
+
