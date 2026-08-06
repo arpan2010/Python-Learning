d18ef934dd83858c731d58050d28a4c7f1d64ed6
arpan <jagtaparpan3@gmail.com>
2026-08-06 23:14:55 +0530

    swapping and counting

---
+++ Variables/2_variable.py
@@ -0,0 +1,12 @@
+# x = 10
+# del x
+# print (x)
+
+# a , b = 10 , 20                 #swapping two variables
+# a , b = b , a
+# print(a ,b)
+
+word = "GeeksforGeeks"
+length = len(word)                  #Counting Characters in a String
+print("length of word is:-" , word)
+print("length of word is:-" , length)
