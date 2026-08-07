c17b9424ee9edeb9196dd1fe9b290c0eca727c89
arpan <jagtaparpan3@gmail.com>
2026-08-07 13:10:13 +0530

    basic mathematical operations

---
+++ 04_Operators/Arithmetic.py
@@ -0,0 +1,9 @@
+a = 15
+b = 4
+print("Addition : ", a + b)
+print("Subtraction : ", a - b)
+print("Multiplication : ", a * b)
+print("Division : ", a / b)
+print("Floor Division : ", a // b)              #removes decimal number
+print("Modulus : ", a % b)
+print("Exponentiation : ", a ** b)              #15*15*15*15 (15 raise to the power 4)
