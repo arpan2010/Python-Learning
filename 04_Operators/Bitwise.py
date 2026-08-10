4c3cfa202c4b67cb751561c76132313d9ffe7b32
arpan <jagtaparpan3@gmail.com>
2026-08-10 14:45:21 +0530

    Added bitwise operators

---
+++ 04_Operators/Bitwise.py
@@ -0,0 +1,33 @@
+# Bitwise AND (&): Compares each bit; if both bits are 1, the result bit is 1.
+a = 5
+b = 3
+print(a & b)
+
+
+# Bitwise OR (|): Compares each bit; if at least one bit is 1, the result bit is 1.
+c = 5
+d = 3
+print(c | d)
+
+
+# Bitwise XOR (^): Compares each bit; if the bits are different, the result bit is 1.
+e = 5
+f = 3
+print(e ^ f)
+
+
+# Bitwise NOT (~): Flips every bit; in Python, ~x is equal to -(x + 1).
+g = 5
+print(~g )
+
+
+# Left Shift (<<): Moves all bits to the left; each shift left by 1 is like multiplying by 2.
+h = 5
+i = 1
+print(h << i)
+
+
+# Right Shift (>>): Moves all bits to the right; each shift right by 1 is like dividing by 2 and taking the floor.
+j = 5
+k = 1
+print(j >> k)
