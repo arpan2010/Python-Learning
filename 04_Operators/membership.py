71e496c5102fb895a7637d9f074bbc26be3ceaef
arpan <jagtaparpan3@gmail.com>
2026-08-12 19:10:05 +0530

    membership operator practice

---
+++ 04_Operators/membership.py
@@ -0,0 +1,23 @@
+fruits = ["mango" , "banana" , "apple" , "watermelon"]
+print("apple" in fruits)
+print("chiku" in fruits)
+
+vegetable = ["spinach" , "coriander" , "potato" , "drumstick"]
+print("coriander" in vegetable)
+print("ladyfinger" in vegetable)
+
+student_name = ["Praful" , "Aditya" , "Hritik" , "Pratik"]
+print("Arpan" in student_name)
+print("Praful" in student_name)
+
+color = ["red" , "grey" , "purple" , "violet"]
+print("black" not in color)
+print("white" not in color)
+print("grey" not in color)
+
+name = "Arpan"
+print("A" in name)
+print("Q" in name)
+print("N" in name)
+print("a" not in name)
+print("f" not in name)
