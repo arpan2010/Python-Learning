1f40de278d80494bdf1e42718e1a93e80c0b57fa
arpan <jagtaparpan3@gmail.com>
2026-08-19 00:55:07 +0530

    used to store numeric values

---
+++ Data Types/Numeric_DataType.py
@@ -0,0 +1,13 @@
+x = 50
+x = 60.5
+x = "Hello World"
+x = ["geeks", "for", "geeks"]
+x = ("geeks", "for", "geeks")
+print (x)
+
+a = 5
+b = 5.90
+c = 9 + 4j
+print (type (a))
+print (type (b))
+print (type(c))
