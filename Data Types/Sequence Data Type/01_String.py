26289d4e318fc3f2364b1144352ca8633772e9fc
arpan <jagtaparpan3@gmail.com>
2026-08-19 01:17:13 +0530

    used to store text data

---
+++ Data Types/Sequence Data Type/01_String.py
@@ -0,0 +1,11 @@
+s = "Welcome to my world"
+print (s)
+print (type(s))
+
+print (s[1])
+print (s[-1])
+
+String:    W    e    l    c    o    m    e
+Positive:  0    1    2    3    4    5    6
+Negative: -7   -6   -5   -4   -3   -2   -1
+           m    y    w    o    r    l    d
