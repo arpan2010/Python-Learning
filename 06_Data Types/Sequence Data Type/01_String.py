3ef21ead5c96b1afcdc0502518ad34ea3cc277cd
arpan <jagtaparpan3@gmail.com>
2026-08-19 01:51:28 +0530

    used to store text data

--- 06_Data Types/Sequence Data Type/01_String.py
+++ 06_Data Types/Sequence Data Type/01_String.py
@@ -9,3 +9,27 @@ String:    W    e    l    c    o    m    e
 Positive:  0    1    2    3    4    5    6
 Negative: -7   -6   -5   -4   -3   -2   -1
            m    y    w    o    r    l    d
+
+City = "Pune"
+print(City)
+
+name = "Arpan"
+print (type(name))
+print (name[0])
+print (name[3])
+print (name[-5])
+
+first_name = "Arpan"
+last_name = "Jagtap"
+print(first_name + last_name)
+
+word = "sunny"
+print (word * 4)
+
+name = "Arpan Arvind Jagtap"
+print(len(name)) # it also counts the space between two words
+
+name = "LINUX"
+print("z" in name)
+print("L" in name)
+print("l" in name)
