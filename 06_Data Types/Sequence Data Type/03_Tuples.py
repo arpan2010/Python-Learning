4f7ccfde8673f8bcc557d868f766274d7aa89f4f
arpan <jagtaparpan3@gmail.com>
2026-08-20 15:55:58 +0530

    used to store multiple items in a single variable

---
+++ 06_Data Types/Sequence Data Type/03_Tuples.py
@@ -0,0 +1,23 @@
+#Once created, tuple elements cannot be modified and are accessed using indexing.
+
+a = ("arpan" , "jagtap")
+print(a)
+print(type(a))
+
+Fruits = ("Apple" , "Grapes" , "Orange" , "Banana")
+print(Fruits)
+print(Fruits[3])
+print(Fruits[-1])
+
+data = ("Arpan" , 38 , 3.80 , 6/7 ) #it will solve the equation🤣🤣
+print(data)
+print(type(data))
+
+solve = (2 * 3 , 4 + 5 , 100/10 , 100 % 10)
+print(solve)
+
+#no append and remove can be use in Tuple
+
+List = ("Arpan" , "Ruthik" , "Aditi" , "Aaniket" , "Jagtap" , "Jadhav")
+print(len(List))
+print(List[1:3])
