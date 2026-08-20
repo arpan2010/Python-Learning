8a7c9bc9e52321394e70958a946aa3b822bb37ee
arpan <jagtaparpan3@gmail.com>
2026-08-20 12:49:18 +0530

    store multiple items in a single variable

---
+++ 06_Data Types/Sequence Data Type/02_List.py
@@ -0,0 +1,45 @@
+a = [4, 5, 6]
+print(a)
+print (type(a))
+
+b = ["geeks" , "For"  , "geeks" , 4 , 5]
+print (b)
+print (b[2])
+print (b[-1])
+print (b[-3])
+
+c = ["arpan" , "python" , 4.5 , 0.101 ]
+print(c)
+print(type(c))
+print(c[1])
+print(c[2])
+
+d = ["Apple" , "Mango" , "Grapes"]
+print(d)
+d[1] = "Banana"
+print(d)
+
+e = ["Car" , "Bike" , "Bicycle"]
+print(e)
+print(type(e))
+e.append("Truck")
+print(e)
+
+f = ["honda" , "hyundai" , "maruti" , "kia"]
+print(f)
+f.remove("maruti")
+print(f)
+
+g = ["apple", "banana", "cherry"]
+print(len(g))
+
+h = ["honda" , "maruti" , "kia"]
+print("honda" in h)
+print("truck" in h)
+
+i = [10, 20, 30, 40, 50]
+print(i[1:4])
+i.append(60)
+print(i)
+print(i[-1:-3])
+print(i[0:2])
