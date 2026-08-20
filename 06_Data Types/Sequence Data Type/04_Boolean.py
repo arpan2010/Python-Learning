759a418ea833220852ebb10e42dc7edd144f2e23
arpan <jagtaparpan3@gmail.com>
2026-08-20 16:20:07 +0530

    represents one of two values: True or False

---
+++ 06_Data Types/Sequence Data Type/04_Boolean.py
@@ -0,0 +1,35 @@
+# is_student = True
+# is_working = False
+# print(is_student)
+# print(is_working)
+# print(type(is_student))
+# print(type(is_working))
+
+# age = 22
+# print (age >= 18)
+
+# age = 15
+# print (age >= 18)
+
+# age = 25
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Child")
+
+# age = 22
+# has_id = True
+# print( age >= 18 and has_id) #True and True → True
+
+# age = 16
+# has_id = True
+# print(age >= 18 or has_id)  #False or True → True
+
+# is_student = True
+# print(not is_student)
+
+is_logged_in = False
+if is_logged_in:
+    print("Welcome!")
+else:
+    print("Please login.")
