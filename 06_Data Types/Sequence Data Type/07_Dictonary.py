1e13a7338c9183704ba8813496ef64183523a843
arpan <jagtaparpan3@gmail.com>
2026-09-02 12:11:21 +0530

    used to store data in key:value pairs

---
+++ 06_Data Types/Sequence Data Type/07_Dictonary.py
@@ -0,0 +1,26 @@
+student = {
+    "name": "Arpan",
+    "age": 22,
+    "course": "MCA"
+
+}
+print(type(student))
+print(student)
+print(student["name"])
+print(student["age"])
+
+car = {
+    "name": "Grand I10",
+    "model": "Sportz",
+    "year": 2018
+
+}
+print(car)
+
+location = {
+    "Country": "India",
+    "State": "Maharashtra"
+}
+print(location["Country"])
+location["City"] = "Pune"
+print(location)
