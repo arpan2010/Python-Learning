44027bcb75c00c3af95691f9c11cd48ae464061b
arpan <jagtaparpan3@gmail.com>
2026-09-14 17:42:40 +0530

    check multiple conditions in a program

--- 07_Conditional Statements/04_If-elif-else Statement.py
+++ 07_Conditional Statements/04_If-elif-else Statement.py
@@ -6,6 +6,7 @@ elif marks >= 70:
 else:
     print("Grade C")
 
+
 Age = int(input("Enter Age :-"))
 if Age >= 21:
     print("Vote and Marriage")
@@ -13,3 +14,17 @@ elif Age >= 18:
     print("Vote")
 else:
     print("Not Adult")
+
+
+Age = int(input("Enter Age :- "))
+if Age >= 22:
+    print("Need to complete masters")
+elif Age <= 16:
+    print("Need to complete schooling")
+elif Age >= 18:
+    print("Need to complete college")
+elif Age >= 5:
+    print("Take admission in school")
+else:
+    print("Life barbaad")
+# In if-elif-else, always check higher ranges first because Python stops at the first True condition.
