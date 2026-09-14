56cc7b0334e9a0735face2070dbfe060fa0be198
arpan <jagtaparpan3@gmail.com>
2026-09-14 17:36:10 +0530

    check multiple conditions in a program

---
+++ 07_Conditional Statements/04_If-elif-else Statement.py
@@ -0,0 +1,15 @@
+marks = 80
+if marks >= 85:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+else:
+    print("Grade C")
+
+Age = int(input("Enter Age :-"))
+if Age >= 21:
+    print("Vote and Marriage")
+elif Age >= 18:
+    print("Vote")
+else:
+    print("Not Adult")
