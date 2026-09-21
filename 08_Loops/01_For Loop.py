d3e021429a2dbdd04204a4bd25c85f12c170237d
arpan <jagtaparpan3@gmail.com>
2026-09-21 15:11:15 +0530

    Loop is used to repeat a block of code multiple times.

---
+++ 08_Loops/01_For Loop.py
@@ -0,0 +1,26 @@
+for i in range(5):
+    print("Hello")
+
+for i in range(1,11):
+    print(i)
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print("python")
+
+for i in range(1, 11):
+    print(i * 2)
+
+for i in range (1,11):
+    print(i * 5)
+
+for i in range ( 1 , 7):         #Addition
+    print(i + 10)
+
+for i in range(1 ,11):            #Subtraction
+    print(i - 2)
+
+for i in range(1 , 6):              #Modulus/Remainder
+    print(i % 2)
