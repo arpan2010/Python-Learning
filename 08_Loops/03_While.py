553f5f08d2084d394b13112f0e3f8afd225c53ca
arpan <jagtaparpan3@gmail.com>
2026-09-22 21:07:08 +0530

    repeatedly executes a block of code as long as the given condition remains true

---
+++ 08_Loops/03_While.py
@@ -0,0 +1,63 @@
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+
+count = 1
+while count <= 3:
+    print("Hello World")
+    count = count + 1
+
+i = 2
+while i <= 10:
+    print(i)
+    i = i + 2
+
+i = 2
+while i <= 20:
+    print(i)
+    i = i + 2
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+i = 10                        #Reverse 10 to 1
+while i >= 1:
+    print(i)
+    i = i - 1
+
+count = 1
+while count <= 3:
+    print("Hello Python")
+    count = count + 1
+
+i = 2                         #Print Even Numbers
+while i <=20:
+    print(i)
+    i = i + 2
+
+i = 1                         #Print Odd Numbers
+while i <= 19:
+    print(i)
+    i = i + 2
+
+i = 1
+while i <= 10:
+    print("5 x", i, "=", 5 * i)
+    i = i + 1
+
+i = 1                             #Divisible by 3 from 1-20
+while i <= 20:
+    if i % 3 ==0:
+        print(i)
+    i = i + 1
+
+
+
