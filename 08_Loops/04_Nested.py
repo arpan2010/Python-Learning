e354478fd2f13c2416b9bbda630215c39bc3dae5
arpan <jagtaparpan3@gmail.com>
2026-09-23 22:00:10 +0530

    Loop inside another loop.

---
+++ 08_Loops/04_Nested.py
@@ -0,0 +1,51 @@
+for i in range(3):
+    for j in range(2):
+        print("Hello")
+
+for i in range(3):
+    for j in range(2):
+        print(i,j)
+
+for i in range(3):
+    for num in range(1,4):
+        print(num)
+
+
+for i in range(2):
+    for j in range(3):
+        print("Hello")
+
+for i in range(2):
+    for j in range(3):
+        print(i,j)
+
+for i in range(2):
+    for j in range(3):
+        print("*")
+
+for i in range(1):
+    for j in range(3):
+        print(1,2,3)
+
+
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, end="")
+    print()
+
+for i in range(1,7):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+for i in range(6,0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+for i in range(1,6):
+    for j in range(i):
+        print(i,end="")
+    print()
+
